5c0e1c6403804a5af86bf0ad60593ec2bc01d6ed
root <root@localhost.localdomain>
2015-09-28 02:01:34 +0800

    v 1.0

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,15 +2,15 @@ class StudentsController < ApplicationController
   layout 'admin'
 
   def index
-    @follows = User.all.pluck(:id) #Follow.where(service_id: @service.id).pluck(:user_id)||User.all.pluck(:id)
-    @orders = User.all.pluck(:id) #Order.where(service_id: @service.id).pluck(:user_id)
+    @follows = Follow.where(service_id: @service.id).pluck(:user_id)||User.all.pluck(:id)
+    @orders = Order.where(service_id: @service.id).pluck(:user_id)
     @users = User.where(id: (@follows + @orders).uniq).paginate(page: params[:page]||1, per_page: 10)
   end
 
 
   def group
-    @follows = User.all.pluck(:id) #Follow.where(service_id: @service.id).pluck(:user_id)||User.all.pluck(:id)
-    @orders = User.all.pluck(:id) #Order.where(service_id: @service.id).pluck(:user_id)
+    @follows = Follow.where(service_id: @service.id).pluck(:user_id)||User.all.pluck(:id)
+    @orders = Order.where(service_id: @service.id).pluck(:user_id)
     @users = User.includes(:profile).where(id: (@follows + @orders).uniq)
     @groups = MassMessageGroup.where(service_id: @service.id)
   end
