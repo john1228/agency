f303fdafa2741f8fbf29048d7a51e4d8269b6fd4
root <root@localhost.localdomain>
2015-10-08 17:36:49 +0800

    font

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module Mxing
     # config.time_zone = 'Central Time (US & Canada)'
     config.active_record.raise_in_transactional_callbacks = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.time_zone = 'Beijing'
     config.i18n.default_locale = 'zh-CN'
