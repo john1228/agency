0f3d834d7dce078eefd4d37e02befdcb60ba4a1e
root <root@localhost.localdomain>
2015-10-10 17:59:48 +0800

    update

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -3,48 +3,65 @@ class ReportController < ApplicationController
 
   def course
     @skus = Sku.where(seller_id: @service.coaches.pluck(:id) << @service.id).pluck(:sku, :course_name)
-    sku = (params[:sku] || @skus.first[0]) rescue ''
-    @date = Date.new((params[:year]||Date.today.year).to_i, (params[:month]||Date.today.month).to_i)
-    @days = []
-    @all = []
-    @coach = []
-    @appointment = []
-    (@date.at_beginning_of_month..@date.at_end_of_month).each { |day|
-      @days << day.day
-      @all << Order.joins(:order_item).where(order_items: {sku: sku}, status: Order::STATUS[:pay], updated_at: day.at_beginning_of_day..day.at_end_of_day).count
-      @coach << Order.joins(:order_item).where(order_items: {sku: sku}, status: Order::STATUS[:pay], updated_at: day.at_beginning_of_day..day.at_end_of_day).count
-      @appointment << Appointment.where(sku: sku, created_at: day.at_beginning_of_day..day.at_end_of_day).count
+    @sku = @skus.first[0] rescue '0'
+    @year = Date.today.year
+    @month = Date.today.month
+  end
+
+
+  def order
+    sku = params[:sku]||(Sku.where(seller_id: @service.coaches.pluck(:id) << @service.id).first.sku rescue '')
+    date = Date.new(params[:year].to_i, params[:month].to_i)
+    @day = []
+    @s_order = []
+    @c_order = []
+    (date.at_beginning_of_month..date.at_end_of_month).each { |everyday|
+      @day << everyday.day
+      @s_order << Order.joins(:order_item).where(order_items: {sku: sku}, service_id: @service.id, status: Order::STATUS[:pay], updated_at: everyday.at_beginning_of_day..everyday.at_end_of_day).count
+      @c_order << Order.joins(:order_item).where(order_items: {sku: sku}, coach_id: @service.coaches.pluck(:id), status: Order::STATUS[:pay], updated_at: everyday.at_beginning_of_day..everyday.at_end_of_day).count
     }
+    @orders = Order.paginate(page: params[:page]||1, per_page: 1) #.joins(:order_item).where(order_items: {sku: sku},
+    #                                          service_id: @service.id,
+    #                                          status: Order::STATUS[:pay],
+    #                                          updated_at: date.at_beginning_of_day..date.at_end_of_day).paginate(page: params[:page]||1, per_page: 1)
+    render layout: false
+  end
 
-    @orders = Order.joins(:order_item).where(order_items: {sku: sku}, status: Order::STATUS[:pay], updated_at: @date.at_beginning_of_day..@date.at_end_of_day).paginate(page: params[:page]||1, per_page: 1)
-    @appointments = Appointment.where(sku: sku, updated_at: @date.at_beginning_of_day..@date.at_end_of_day).order(id: :desc).paginate(page: params[:page]||1, per_page: 1)
-    respond_to do |format|
-      format.html
-      format.json { render json: {day: @days, all: @all, coach: @coach} }
-    end
+  def appointment
+    sku = params[:sku]||(Sku.where(seller_id: @service.coaches.pluck(:id) << @service.id).first.sku rescue '')
+    date = Date.new(params[:year].to_i, params[:month].to_i)
+    @day = []
+    @appointment = []
+    (date.at_beginning_of_month..date.at_end_of_month).each { |everyday|
+      @day << everyday.day
+      @appointment << Appointment.where(coach_id: @service.coaches.pluck(:id), sku: sku).count
+    }
+    @appointments = Appointment.where(coach_id: @service.coaches.pluck(:id), sku: sku).order(id: :desc).paginate(page: params[:page]||1, per_page: 1)
+    render layout: false
+  end
 
+  def sale
+    sku = params[:sku]||(Sku.where(seller_id: @service.coaches.pluck(:id) << @service.id).first.sku rescue '')
+    date = Date.new(params[:year].to_i, params[:month].to_i)
+    @day = []
+    @s_sale = []
+    @c_sale = []
+    (date.at_beginning_of_month..date.at_end_of_month).each { |everyday|
+      @day << everyday.day
+      @s_sale << (Order.joins(:order_item).where(order_items: {sku: sku}, service_id: @service.id, status: Order::STATUS[:pay], updated_at: everyday.at_beginning_of_day..everyday.at_end_of_day).sum(:total)).to_i
+      @c_sale << (Order.joins(:order_item).where(order_items: {sku: sku}, coach_id: @service.coaches.pluck(:id), status: Order::STATUS[:pay], updated_at: everyday.at_beginning_of_day..everyday.at_end_of_day).sum(:total)).to_i
+    }
+    @orders = Order.joins(:order_item).where(order_items: {sku: sku},
+                                             service_id: @service.id,
+                                             status: Order::STATUS[:pay],
+                                             updated_at: date.at_beginning_of_day..date.at_end_of_day).paginate(page: params[:page]||1, per_page: 1)
+    render layout: false
   end
 
   def coach
     @coaches = @service.coaches.pluck(:id, 'profiles.name')
-    coach = (params[:coach]||@coaches.first[0]) rescue 0
-    @date = Date.new((params[:year]||Date.today.year).to_i, (params[:month]||Date.today.month).to_i)
-    @days = []
-    @coach = []
-    @appointment = []
-    (@date.at_beginning_of_month..@date.at_end_of_month).each { |day|
-      @days << day.day
-      @coach << Order.where(coach_id: coach, status: Order::STATUS[:pay], updated_at: day.at_beginning_of_day..day.at_end_of_day).count
-      @appointment << Appointment.where(coach_id: coach, created_at: day.at_beginning_of_day..day.at_end_of_day).count
-    }
-
-    @orders = Order.where(coach_id: coach, status: Order::STATUS[:online]).paginate(page: params[:page]||1, per_page: 1)
-    @appointments = Appointment.where(coach_id: coach).paginate(page: params[:page]||1, per_page: 1)
-
-    respond_to do |format|
-      format.html
-      format.json { render json: {day: @days, all: @all, coach: @coach} }
-    end
-
+    @coach = @service.coaches.first.id rescue 0
+    @year = Date.today.year
+    @month = Date.today.month
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,10 @@ Rails.application.routes.draw do
   get 'report' => 'report#course'
   get 'report/coach' => 'report#coach'
 
+  post 'report/order' => 'report#order'
+  post 'report/appointment' => 'report#appointment'
+  post 'report/sale' => 'report#sale'
+
   get 'finance/transfer' => 'finance#transfer_new'
   post 'finance/transfer' => 'finance#transfer_create', as: :submit_transfer
   post 'finance/batch_transfer' => 'finance#batch_transfer', as: :batch_transfer
