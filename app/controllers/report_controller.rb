eda6b54c3b56b78b8f30a7d53a41f80e07a0f92b
root <root@localhost.localdomain>
2015-10-10 12:27:46 +0800

    update

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -3,6 +3,7 @@ class ReportController < ApplicationController
 
   def course
     @skus = Sku.where(seller_id: @service.coaches.pluck(:id) << @service.id).pluck(:sku, :course_name)
+    sku = params[:sku] || @skus.first[0]
     @date = Date.new((params[:year]||Date.today.year).to_i, (params[:month]||Date.today.month).to_i)
     @days = []
     @all = []
@@ -10,25 +11,13 @@ class ReportController < ApplicationController
     @appointment = []
     (@date.at_beginning_of_month..@date.at_end_of_month).each { |day|
       @days << day.day
-      if params[:sku].blank?
-        @all << Order.where(service_id: @service.id, status: Order::STATUS[:pay], updated_at: day.at_beginning_of_day..day.at_end_of_day).count
-        @coach << Order.where(service_id: @service.id, status: Order::STATUS[:pay], updated_at: day.at_beginning_of_day..day.at_end_of_day).count
-        @appointment << Appointment.where(coach_id: @service.coaches.pluck(:id), created_at: day.at_beginning_of_day..day.at_end_of_day).count
-      else
-        @all << Order.joins(:order_item).where(order_items: {sku: params[:sku]}, status: Order::STATUS[:pay], updated_at: day.at_beginning_of_day..day.at_end_of_day).count
-        @coach << Order.joins(:order_item).where(order_items: {sku: params[:sku]}, status: Order::STATUS[:pay], updated_at: day.at_beginning_of_day..day.at_end_of_day).count
-        @appointment << Appointment.where(sku: params[:sku], created_at: day.at_beginning_of_day..day.at_end_of_day).count
-      end
-
-
+      @all << Order.joins(:order_item).where(order_items: {sku: sku}, status: Order::STATUS[:pay], updated_at: day.at_beginning_of_day..day.at_end_of_day).count
+      @coach << Order.joins(:order_item).where(order_items: {sku: sku}, status: Order::STATUS[:pay], updated_at: day.at_beginning_of_day..day.at_end_of_day).count
+      @appointment << Appointment.where(sku: sku, created_at: day.at_beginning_of_day..day.at_end_of_day).count
     }
-    if params[:sku].blank?
-      @orders = Order.where(service_id: @service.id, status: Order::STATUS[:pay]).paginate(page: params[:page]||1, per_page: 1)
-      @appointments = Appointment.where(coach: @service.coaches).order(id: :desc).paginate(page: params[:page]||1, per_page: 1)
-    else
-      @orders = Order.joins(:order_item).where(order_items: {sku: params[:sku]}, status: Order::STATUS[:pay]).paginate(page: params[:page]||1, per_page: 1)
-      @appointments = Appointment.where(sku: params[:sku]).order(id: :desc).paginate(page: params[:page]||1, per_page: 1)
-    end
+
+    @orders = Order.joins(:order_item).where(order_items: {sku: sku}, status: Order::STATUS[:pay], updated_at: @date.at_beginning_of_day..@date.at_end_of_day).paginate(page: params[:page]||1, per_page: 1)
+    @appointments = Appointment.where(sku: sku, updated_at: @date.at_beginning_of_day..@date.at_end_of_day).order(id: :desc).paginate(page: params[:page]||1, per_page: 1)
     respond_to do |format|
       format.html
       format.json { render json: {day: @days, all: @all, coach: @coach} }
@@ -38,14 +27,22 @@ class ReportController < ApplicationController
 
   def coach
     @coaches = @service.coaches.pluck(:id, 'profiles.name')
-    @date = Date.today
+    @date = Date.new((params[:year]||Date.today.year).to_i, (params[:month]||Date.today.month).to_i)
     @days = []
-    @all = []
     @coach = []
     @appointment = []
     (@date.at_beginning_of_month..@date.at_end_of_month).each { |day|
-      count = rand(100)
       @days << day.day
+      if params[:coach].blank?
+        @all << Order.where(service_id: @service.id, status: Order::STATUS[:pay], updated_at: day.at_beginning_of_day..day.at_end_of_day).count
+        @coach << Order.where(service_id: @service.id, status: Order::STATUS[:pay], updated_at: day.at_beginning_of_day..day.at_end_of_day).count
+        @appointment << Appointment.where(coach_id: @service.coaches.pluck(:id), created_at: day.at_beginning_of_day..day.at_end_of_day).count
+      else
+        @all << Order.joins(:order_item).where(order_items: {sku: params[:sku]}, status: Order::STATUS[:pay], updated_at: day.at_beginning_of_day..day.at_end_of_day).count
+        @coach << Order.joins(:order_item).where(order_items: {sku: params[:sku]}, status: Order::STATUS[:pay], updated_at: day.at_beginning_of_day..day.at_end_of_day).count
+        @appointment << Appointment.where(sku: params[:sku], created_at: day.at_beginning_of_day..day.at_end_of_day).count
+      end
+
       @coach << rand(count)
       @appointment << rand(count)
     }
