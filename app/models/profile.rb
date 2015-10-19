7147ae5258a3fb9173663312db923ac051cc2f35
root <root@localhost.localdomain>
2015-10-19 11:32:23 +0800

    update

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -36,14 +36,14 @@ class Profile < ActiveRecord::Base
 
   private
   def regist_to_easemob
-    # easemob_token = Rails.cache.fetch('mob')
-    # Faraday.post do |req|
-    #   req.url "#{MOB['host']}/users"
-    #   req.headers['Content-Type'] = 'application/json'
-    #   req.headers['Authorization'] = "Bearer #{easemob_token}"
-    #   req.body = "{\"username\": \"#{mxid}\", \"password\": \"123456\", \"nickname\": \"#{name}\"}"
-    # end
-    # MessageJob.set(wait: 1.minute).perform_later(id, MESSAGE['欢迎语'])
+    easemob_token = Rails.cache.fetch('mob')
+    Faraday.post do |req|
+      req.url "#{MOB['host']}/users"
+      req.headers['Content-Type'] = 'application/json'
+      req.headers['Authorization'] = "Bearer #{easemob_token}"
+      req.body = "{\"username\": \"#{mxid}\", \"password\": \"123456\", \"nickname\": \"#{name}\"}"
+    end
+    MessageJob.set(wait: 1.minute).perform_later(id, MESSAGE['欢迎语'])
   end
 
   def delete_from_easemob
