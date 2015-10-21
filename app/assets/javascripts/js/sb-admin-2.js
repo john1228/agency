e2988dbae0d7a1f042835475d28f4dbbccda5fd2
root <root@localhost.localdomain>
2015-10-21 12:14:48 +0800

    update

--- app/assets/javascripts/js/sb-admin-2.js
+++ app/assets/javascripts/js/sb-admin-2.js
@@ -47,7 +47,7 @@ $(function () {
 
     var url = window.location;
     var element = $('ul.nav a').filter(function () {
-        return this.href == url;
+        return this.href == url || (url.href.indexOf(this.href) == 0 && url.href.indexOf('?') > 0);
     }).addClass('active').parent().parent().addClass('in').parent();
     if (element.is('li')) {
         element.addClass('active');
