be22b3fe4ab1b2f4ca0f9d9a7df768180800b05c
femto <femtowin@gmail.com>
2015-11-13 16:33:08 +0800

    w

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -3,17 +3,17 @@ class AdminUsersController < InheritedResources::Base
 
   def index
 
-    @q = AdminUser.where(:client_id => current_user.client_id).ransack(params[:q])
+    @q = AdminUser.where(:client_id => current_user.client_id).where("role > 4").ransack(params[:q])
     @admin_users = @q.result.paginate(page: params[:page]||1, per_page: 5).order("updated_at desc")
 
   end
 
   def create
-    result, @admin_user = AdminUser.create_from_input(admin_user_params, current_user.client_id)
+    @admin_user = AdminUser.new(admin_user_params, current_user.client_id)
 
-    if result
+    if @admin_user.save
       @success = true
-      flash[:success] = "成功创建会员"
+      flash[:success] = "成功创建工作人员"
       redirect_to admin_users_path
     else
       #flash[:error] = "xxx"
@@ -26,7 +26,7 @@ class AdminUsersController < InheritedResources::Base
     @admin_user.assign_attributes(admin_user_params)
     if @admin_user.save
       @success = true
-      flash[:success] = "成功修改会员"
+      flash[:success] = "成功创建工作人员"
       redirect_to admin_users_path
     else
       #flash[:error] = "xxx"
@@ -37,8 +37,12 @@ class AdminUsersController < InheritedResources::Base
   private
 
   def admin_user_params
-    params.require(:admin_user).permit(:reg_type, :avatar, :name, :gender, :service_id, :client_id, :mobile, :source, :birthday, :address, :remark,
-                                              :province,:city)
+    fileds = [:reg_type, :avatar, :name, :gender, :service_id, :client_id, :mobile, :source, :birthday, :address, :remark,
+        :province,:city]
+    if params[:password].present?
+      fileds << :password
+    end
+    params.require(:admin_user).permit(fileds)
   end
 end
 
