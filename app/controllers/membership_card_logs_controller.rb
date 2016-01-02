3b7fd3c5a48835c5add1d74589a3b2cdb9ff079b
root <313423716@qq.com>
2016-01-02 18:12:31 +0800

    fix bug

--- app/controllers/membership_card_logs_controller.rb
+++ app/controllers/membership_card_logs_controller.rb
@@ -2,7 +2,7 @@ class MembershipCardLogsController < ApplicationController
   layout 'admin'
 
   def index
-    @membership_card = MemberShipCard.find(params[:membership_card_id])
+    @membership_card = MembershipCard.find(params[:membership_card_id])
     @logs = @membership_card.logs.paginate(page: params[:page]||1, per_page: 10)
   end
 end
