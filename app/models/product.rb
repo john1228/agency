01983178541468505945111c1183371d0a38e50f
root <313423716@qq.com>
2016-01-02 16:00:31 +0800

    fix bug

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,26 +3,34 @@ class Product < ActiveRecord::Base
   has_one :prop, class: ProductProp
   attr_accessor :service_id, :market_price, :selling_price, :store, :limit, :seller_id
   belongs_to :card_type, class: MembershipCardType
+  has_one :service, through: :sku
 
   validates_presence_of :card_type_id, message: '请选择卡'
   validates_presence_of :name, message: '请输入出售卡的卡名'
   validates_presence_of :description, message: '请输入对卡的说明'
 
-  mount_uploaders :image, ImagesUploader
-  after_create :generate_sku
-
-
+  mount_uploaders :image, ProductImagesUploader
   accepts_nested_attributes_for :prop
-
-  private
-  def generate_sku
-    GenerateSkuJob.perform_now(self, {
-                                       service_id: service_id,
-                                       market_price: market_price,
-                                       selling_price: selling_price,
-                                       store: store||'-1',
-                                       limit: limit||'-1',
-                                       seller_id: seller_id
-                                   })
+  after_create :build_default_sku
+  protected
+  def build_default_sku
+    service = Service.find(service_id)
+    create_sku(
+        sku: 'SM'+'-' + '%06d' % id + '-' + '%06d' % (service.id),
+        course_type: card_type_id,
+        course_name: name,
+        course_cover: image.first.url,
+        seller: service.profile.name,
+        seller_id: seller_id||service_id,
+        service_id: service_id,
+        market_price: market_price,
+        selling_price: selling_price,
+        store: store,
+        limit: limit,
+        address: service.profile_address,
+        coordinate: (service.place.lonlat rescue 'POINT(0 0)'),
+        status: 'online',
+        sku_type: 'card'
+    )
   end
 end
