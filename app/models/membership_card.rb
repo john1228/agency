35d7e1a9b156080412b24c2ac62b00701f9e92a8
root <313423716@qq.com>
2016-01-02 16:03:05 +0800

    fix bug

--- app/models/membership_card.rb
+++ app/models/membership_card.rb
@@ -1,6 +1,7 @@
 class MembershipCard < ActiveRecord::Base
   enum card_type: [:stored, :measured, :clocked, :coach]
   belongs_to :member
+  belongs_to :service
   has_many :logs, class: MembershipCardLog, dependent: :destroy
   class << self
     def card_type_for_select
