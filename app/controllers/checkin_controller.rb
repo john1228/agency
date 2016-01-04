7166baa2054fdd583d2d1341ad8814dcffc65a4f
root <313423716@qq.com>
2016-01-04 18:54:24 +0800

    fix bug

--- app/controllers/checkin_controller.rb
+++ app/controllers/checkin_controller.rb
@@ -3,7 +3,7 @@ class CheckinController < ApplicationController
 
   def index
     @member = Member.where(client_id: current_user.client_id).find_by(id: params[:member])
-    @members = Member.where(client_id: current_user.client_id).pluck(:name, :id)
+    @members = Member.full.where(client_id: current_user.client_id).pluck(:name, :id)
     @cards = MembershipCard.where(member: @member)
   end
 
