c364821ea4cc45acb333a7cda8282972a2e48268
root <313423716@qq.com>
2016-01-06 21:10:15 +0800

    fix bug

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,7 +2,7 @@ class Member < ActiveRecord::Base
   enum gender: [:male, :female]
   enum origin: [:mxing, :input]
   enum member_type: [:associate, :full, :coach]
-  validates :name, :birthday, :gender, :mobile, :service_id, :province, :city, :area, :address, :avatar, presence: true
+  #validates :name, :birthday, :gender, :mobile, :service_id, :province, :city, :area, :address, :avatar, presence: true
   mount_uploader :avatar, ProfileUploader
 
   belongs_to :service
