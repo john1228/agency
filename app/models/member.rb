fb9c710d89bfe232785fd8310400ecf51c560e6a
root <313423716@qq.com>
2016-01-11 19:26:06 +0800

    fix bug

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ActiveRecord::Base
-  enum gender: [:male, :female]
+  #enum gender: [:male, :female]
   enum origin: [:mxing, :input]
   enum member_type: [:associate, :full, :coach]
   #validates :name, :birthday, :gender, :mobile, :service_id, :province, :city, :area, :address, :avatar, presence: true
