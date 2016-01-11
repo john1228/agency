86298563ac064310374abbb4e6a3cf1d413e60e9
root <313423716@qq.com>
2016-01-11 18:32:37 +0800

    fix bug

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,7 +15,7 @@ class Product < ActiveRecord::Base
   protected
   def build_default_sku
     service = Service.find(service_id)
-    seller = Service.find_by(seller_id)
+    seller = Coach.find(seller_id)
     create_sku(
         sku: 'SM'+'-' + '%06d' % id + '-' + '%06d' % (service.id),
         course_type: card_type.card_type,
@@ -28,7 +28,7 @@ class Product < ActiveRecord::Base
         selling_price: selling_price,
         store: store,
         limit: limit,
-        address: (service.profile.province||'') + (service.profile.city||'') + (service.profile.area||'') + (service.profile.address|''),
+        address: (service.profile.province||'') + (service.profile.city||'') + (service.profile.area||'') + (service.profile.address||''),
         coordinate: (service.place.lonlat rescue 'POINT(0 0)'),
         status: 'online',
         sku_type: 'card'
