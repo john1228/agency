6d6e45b567e4fb5185b258138dac5fbce46937e6
root <313423716@qq.com>
2016-01-13 01:06:59 +0800

    fix bug

--- app/controllers/membership_cards_controller.rb
+++ app/controllers/membership_cards_controller.rb
@@ -27,7 +27,7 @@ class MembershipCardsController < ApplicationController
 
   def active
     membership_card = MembershipCard.find_by(id: params[:id])
-    membership_card.open = Date.todayd
+    membership_card.open = Date.today
     if membership_card.active!
       redirect_to action: :index, flash: "激活成功"
     else
