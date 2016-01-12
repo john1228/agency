3e48330a456cdb319ece2fb1cd748f850273a65f
root <313423716@qq.com>
2016-01-12 17:58:48 +0800

    fix bug

--- db/migrate/20151231112836_change_columns_of_membership_card.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnsOfMembershipCard < ActiveRecord::Migration
-  def change
-    rename_column :membership_card_types, :latest_delay_days, :delay_days
-    rename_column :membership_card_types, :count, :value
-  end
-end
