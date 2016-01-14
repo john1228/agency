9dc475ff74d11a8af7d1d86489f88eef19fcfc6a
root <313423716@qq.com>
2016-01-14 19:02:40 +0800

    fix bug

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,14 +4,19 @@ class AdminUser < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :service #we may manage one service
   belongs_to :client
-  include AASM
 
-  ROLE = {super: 0, service: 1, cms: 2, market: 3, operator: 4}
+
   enum gender: [:male, :female]
-  enum role: [:super, :admin, :cms, :market, :operator, :superadmin, :sales, :front_desk, :finance, :store_manager, :store_admin]
+  enum role: [:super, :admin, :cms, :market, :operator, :superadmin, :sale, :front_desk, :finance, :store_manager, :store_admin]
   mount_uploader :avatar, PhotosUploader
+  validates_presence_of :email, message: '邮箱必须输入'
+  validates_presence_of :password, message: '密码必须输入'
+  validates_presence_of :role, message: '身份必须选择'
+  validates_presence_of :name, message: '名字必须输入', if: Proc.new { |user| user.sale? || user.front_desk? || user.finance? || user.store_manager? || user.store_admin? }
+  validates_presence_of :service_id, message: '门店必须选择', if: Proc.new { |user| user.sale? || user.front_desk? || user.finance? || user.store_manager? || user.store_admin? }
 
 
+  include AASM
   aasm :status do
     state :approving, :initial => true
     state :rejected
